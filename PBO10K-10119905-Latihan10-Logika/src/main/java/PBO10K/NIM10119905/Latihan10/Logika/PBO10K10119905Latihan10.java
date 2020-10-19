9a9d7c40af902c6f4eb114b057d8d641c0f41b8e
arumayara <72974847+arumayara@users.noreply.github.com>
2020-10-19 07:16:40 +0700

    Add files via upload

---
+++ PBO10K-10119905-Latihan10-Logika/src/main/java/PBO10K/NIM10119905/Latihan10/Logika/PBO10K10119905Latihan10.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO10K.NIM10119905.Latihan10.Logika;
+
+/**
+ *
+ * @author 
+ * NAMA    : Abraham Rumayara
+ * KELAS   : PBO10K
+ * NIM     : 10119905
+ * Deskripsi Program : Program ini menampilkan Logika
+ * 
+ */
+public class PBO10K10119905Latihan10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >=17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
